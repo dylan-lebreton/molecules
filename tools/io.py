4707f592d335ad9c99a91302b0d212e11bad2e07
dylan.lebreton <dylan.lebreton@insa-toulouse.fr>
2023-06-20 11:04:45 +0200

    Dylan: definition of routine to automate the export to Kaggle CSV format.

---
+++ tools/io.py
@@ -0,0 +1,8 @@
+def to_kaggle_csv(dataframe, csv_file_path: str = r"./kaggle_submission.csv",
+                  molecule_id_column = "molecule_id",
+                  molecule_prediction_column = "prediction"):
+    result = dataframe[[molecule_id_column, molecule_prediction_column]].copy(deep=True)
+    result = result.drop_duplicates(subset=[molecule_id_column]).reset_index(drop=True)
+    result = result.sort_values(by=molecule_id_column).reset_index(drop=True)
+    result.rename(columns={molecule_prediction_column: "prediction"})
+    result.to_csv(csv_file_path, index=False)
