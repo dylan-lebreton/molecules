5d9cb7e62075a9e7ec575d312e23e3c4a94fc079
dylan.lebreton <dylan.lebreton@insa-toulouse.fr>
2023-06-20 11:15:59 +0200

    Dylan: - correction of to_kaggle_csv routine - update of regression study to include generation of kaggle csv - add of the correspondant csv to data/kaggle

--- studies/regression.py
+++ studies/regression.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
-from sklearn.linear_model import LinearRegression
 
-from data.loader import load_train_test
+from data.loader import load_train_test, load
+from tools.io import to_kaggle_csv
 
 tqdm.pandas()
 
@@ -28,7 +29,6 @@ plt.ylabel("Molecule energy")
 plt.scatter(train['n_atoms'], train['molecule_energy'])
 plt.show()
 
-
 # we see that there is a correlation between the number of atoms and the energy of the molecule
 # we can therefore use this feature to predict the energy of the molecule
 
@@ -44,3 +44,21 @@ test['molecule_energy_prediction'] = model.predict(test[['n_atoms_normalized']])
 
 print('RMSE on train : ', np.sqrt(mean_squared_error(train['molecule_energy'], train['molecule_energy_prediction'])))
 print('RMSE on test : ', np.sqrt(mean_squared_error(test['molecule_energy'], test['molecule_energy_prediction'])))
+
+# make prediction on real test data
+test = load(molecules_folder_path=r"../data/atoms/test",
+            energies_file_path=r"../data/energies/test.csv",
+            already_saved_file_path=r"../data/test.csv")
+
+# computation of the number of atoms per molecule for the real test data
+test_molecule_id_value_counts = test['molecule_id'].value_counts().to_dict()
+test['n_atoms'] = test['molecule_id'].progress_apply(lambda molecule_id: test_molecule_id_value_counts[molecule_id])
+
+# keep only per molecule
+test = test.drop_duplicates(subset=['molecule_id']).reset_index(drop=True).drop(columns=['atom_name', 'x', 'y', 'z'])
+
+test[['n_atoms_normalized']] = scaler.transform(test[['n_atoms']])
+test['molecule_energy'] = model.predict(test[['n_atoms_normalized']])
+to_kaggle_csv(test, csv_file_path=r"../data/kaggle/regression.csv",
+              molecule_id_column='molecule_id',
+              molecule_prediction_column='molecule_energy')
--- tools/io.py
+++ tools/io.py
@@ -4,5 +4,5 @@ def to_kaggle_csv(dataframe, csv_file_path: str = r"./kaggle_submission.csv",
     result = dataframe[[molecule_id_column, molecule_prediction_column]].copy(deep=True)
     result = result.drop_duplicates(subset=[molecule_id_column]).reset_index(drop=True)
     result = result.sort_values(by=molecule_id_column).reset_index(drop=True)
-    result.rename(columns={molecule_prediction_column: "prediction"})
-    result.to_csv(csv_file_path, index=False)
+    result.rename(columns={molecule_prediction_column: "prediction"}, inplace=True)
+    result[["prediction"]].to_csv(csv_file_path, index=False)
