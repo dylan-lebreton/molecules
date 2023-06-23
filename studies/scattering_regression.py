e423ee3d3e30d6bea0fb5582c598a74d01024a56
dylan.lebreton <dylan.lebreton@insa-toulouse.fr>
2023-06-23 13:10:28 +0200

    Dylan : add of scattering coefficients files

---
+++ studies/scattering_regression.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+from data.loader import load
+
+# retrieve of train data
+train = load(molecules_folder_path=r"../data/atoms/train",
+             energies_file_path=r"../data/energies/train.csv",
+             already_saved_file_path=r"./train.csv")
+
+print("debug")
